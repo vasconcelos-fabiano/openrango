1f748bfdda1dd09d03f96caa7c26990e873ba35b
Fabiano Vasconcelos <fvasconcelos@gmail.com>
2026-08-06 10:15:27 -0300

    Melhoria

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
   {
     path: '',
+    component: Login,
+  },
+  {
+    path: 'login',
     component: Login,
   },
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './pages/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('frontend');
+
+  loggedIn = false;
 }
