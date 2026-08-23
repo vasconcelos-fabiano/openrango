e5fb104a0cf36fe8239e36d022e9c767b090eb4a
Fabiano Vasconcelos <fvasconcelos@gmail.com>
2026-08-23 19:56:06 -0300

    Evolui banco de dados e inicia backend FastAPI com Docker

---
+++ database/migrations/003_classify_legacy_products.sql
@@ -0,0 +1,11 @@
+UPDATE produtos
+SET categoria = 'b'
+WHERE id IN (15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29);
+
+UPDATE produtos
+SET categoria = 'c'
+WHERE id IN (47, 48, 49, 50);
+
+UPDATE produtos
+SET categoria = 'd'
+WHERE id IN (52, 53, 54, 55, 56);
