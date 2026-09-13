4b498aed7872e71d97ab502f1c9135e47471e4ff
Fabiano Vasconcelos <fvasconcelos@gmail.com>
2026-09-13 15:20:56 -0300

    Build inventory product entry form

--- frontend/src/app/pages/estoque/estoque.ts
+++ frontend/src/app/pages/estoque/estoque.ts
@@ -6,4 +6,6 @@ import { Component } from "@angular/core";
   templateUrl: "./estoque.html",
   styleUrl: "./estoque.css",
 })
-export class Estoque {}
+export class Estoque {
+  gtinEnabled = false;
+}
